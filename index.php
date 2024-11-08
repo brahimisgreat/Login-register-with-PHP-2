c489406e1a46b7d9386c2a821f430d6c8f7cb884
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-11-07 22:05:18 -0500

    fdfd

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
-<?php
-    $colors = ["red", "green", "blue", "yellow", "purple", "orange"];
-    $colorsw = array("john", "doe", "jane", "smith", "james", "bond");
-
-    print_r($colors);
-?>
-<html>
+<html lang="en">
 <head>
-    <title>Login Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <h2><?= $rand_color ?></h2>
-    <form action="login.php" method="post">
+<h1>Login</h1>
+    <form action="index.php" method="post">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required><br><br>
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br><br>
         <input type="submit" value="Login">
-
+        <div>
+            Don't have an account?  <a href="register.php">sign Up Now</a>
+        </div>
     </form>
 </body>
 </html>
